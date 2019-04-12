b1d81dd8e503c8760d91fb32b3a0fe6483e34743
KiyoM99 <47841105+KiyoM99@users.noreply.github.com>
2019-04-12 23:48:14 +0800

    Add files via upload

---
+++ hw3/hw3_test.sh
@@ -0,0 +1,3 @@
+wget 'https://github.com/KiyoM99/ML2019SPRING/releases/download/0.0.0/cnn_21.pt' -O cnn_21.pt
+wget 'https://github.com/KiyoM99/ML2019SPRING/releases/download/0.0.0/cnn_48.pt' -O cnn_48.pt
+python test.py $1 $2
